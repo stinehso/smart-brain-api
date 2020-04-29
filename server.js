1ecc22060f49973afec6efc12a9eeead8634759a
Stine <stinehso@gmail.com>
2020-04-29 18:11:16 +0200

    update port

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.post('/imageurl', image.handleApiCall);
 
 
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 });
